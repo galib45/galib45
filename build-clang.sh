f235bb2e50bbe2daca68e594c702c6616dc63022
Asadul Al Galib <asadulgalibvat69@gmail.com>
2024-08-27 14:58:13 +0600

    update build script and workflow

--- build-clang.sh
+++ build-clang.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# modified from https://github.com/wasmerio/llvm-custom-builds/blob/master/build.sh
 
 # Display all commands before executing them.
 set -o errexit
@@ -21,14 +22,15 @@ mkdir -p install
 # Run `cmake` to configure the project.
 cmake \
   -G Ninja \
-  -DCMAKE_BUILD_TYPE=MinSizeRel \
+  -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX="/" \
-  -DCMAKE_C_COMPILER=clang \
-  -DCMAKE_CXX_COMPILER=clang++ \
+  -DCMAKE_C_COMPILER="clang -static" \
+  -DCMAKE_CXX_COMPILER="clang++ -static" \
   -DLLVM_USE_LINKER=lld \
   -DLLVM_ENABLE_PROJECTS="clang;lld" \
   -DLLVM_ENABLE_TERMINFO=OFF \
   -DLLVM_ENABLE_ZLIB=OFF \
+  -DLLVM_ENABLE_ZSTD=OFF \
   -DLLVM_INCLUDE_DOCS=OFF \
   -DLLVM_INCLUDE_EXAMPLES=OFF \
   -DLLVM_INCLUDE_GO_TESTS=OFF \
@@ -36,13 +38,13 @@ cmake \
   -DLLVM_INCLUDE_TOOLS=ON \
   -DLLVM_INCLUDE_UTILS=OFF \
   -DLLVM_OPTIMIZED_TABLEGEN=ON \
-  -DLLVM_ENABLE_RUNTIMES="compiler-rt" \
+  #-DLLVM_ENABLE_RUNTIMES="compiler-rt" \
   -DLLVM_TARGETS_TO_BUILD="X86" \
   ../llvm
 
 # Showtime!
-cmake --build . --config MinSizeRel -j $(nproc)
-DESTDIR=destdir cmake --install . --strip --config MinSizeRel
+cmake --build . --config Release -j $(nproc)
+DESTDIR=destdir cmake --install . --strip --config Release
 
 # move usr/bin/* to bin/ or llvm-config will be broken
 if [ ! -d destdir/bin ];then
